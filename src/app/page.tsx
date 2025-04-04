8aec7cef331081bb74b7d1bf6974688023798306
袁澧林 <46401679+Angela980918@users.noreply.github.com>
2025-04-04 11:32:46 +0800

    更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { PrismaClient } from "../../node_modules/.prisma/client/index";
+
 export default function Home() {
-  return (
-    <div>Home</div>
-  );
+  const prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  });
+
+  console.log("prisma", prisma);
+
+  return <div>Home</div>;
 }
